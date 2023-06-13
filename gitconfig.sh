c9be083bc7ce6836ffe9cacccf587085257a1d57
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-06-13 13:07:39 +0530

    Add: gitconfig script

---
+++ gitconfig.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+#Get Username
+echo "Github username:"
+read -r USERNAME
+
+#Get Github ID
+GET_ID() { curl -s https://api.github.com/users/"$USERNAME" | jq '.id'; }
+ID=$(GET_ID)
+
+#Set Git Global Config
+git config --global user.email "$ID+$USERNAME@users.noreply.github.com"
+git config --global user.name "$USERNAME"
