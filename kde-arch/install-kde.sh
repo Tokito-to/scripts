40447e68a9d0dd25e852f2b3da258f719af0e60d
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-07-03 10:52:38 +0530

    Add:sudo package & configure it

--- kde-arch/install-kde.sh
+++ kde-arch/install-kde.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Exit on any error
-set -ex
+set -e
 clear
 
 err() {
@@ -19,11 +19,11 @@ if [[  $EUID -ne 0 ]]; then
         exit 0 ;fi
 
 # Chech Internet Connection
-if ! ping -c3 archlinux.org ;then
+if ! ping -c1 archlinux.org ;then
 err "Connect to Internet & try again!" ;fi
 
 # Configuration
-prompt "Standard Username [Asuna]: "
+prompt "Standard Username [asuna]: "
 read USERNAME
 USERNAME=${USERNAME:-asuna}
 
@@ -36,10 +36,6 @@ prompt "User Password [yuuki]: "
 read -s USER_PASSWORD
 USER_PASSWORD=${USER_PASSWORD:-yuuki}
 
-prompt "Pacman Mirror Country [India]: "
-read COUNTRY
-COUNTRY=${COUNTRY:-India}
-
 # Configuration
 echo ""
 echo ""
@@ -47,11 +43,17 @@ printf "%-16s\t%-16s\n" "CONFIGURATION" "VALUE"
 printf "%-16s\t%-16s\n" "Username:" "$USERNAME"
 printf "%-16s\t%-16s\n" "User as Root:" "$USER_AS_ROOT"
 printf "%-16s\t%-16s\n" "User Password:" "`echo \"$USER_PASSWORD\" | sed 's/./*/g'`"
-printf "%-16s\t%-16s\n" "Mirror Country: " "$COUNTRY"
 echo ""
 prompt "Proceed? [y/N]: "
 read PROCEED
 [[ "$PROCEED" != "y" ]] && err "User chose not to proceed. Exiting."
+
+
+set -x
+
+# Instal and Setup sudo
+pacman -Sy --noconfirm sudo
+groupadd sudo
 
 # Setup user
 useradd -m "$USERNAME"
@@ -59,23 +61,15 @@ useradd -m "$USERNAME"
 if [ "$USER_AS_ROOT" = "Yes" ];then
 usermod -aG sudo "$USERNAME" ;fi
 
+# Don't ask passwd for sudo superuser # only for $USERNAME
+echo "## Allow $USERNAME to execute any root command
+%$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
 # Pacman Configuration
 sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 4/" "/etc/pacman.conf"
 sed -i "s/#Color/Color/" "/etc/pacman.conf"
-sed -i "s/#[multilib]/[multilib]/" "/etc/pacman.conf"
-sed -i "s/#Include = \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/mirrorlist/" "/etc/pacman.conf"
-
-# Update Mirrors
-export TMPFILE="$(mktemp)"
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-curl -L "https://archlinux.org/mirrorlist/all/" > 'mirrorlist'
-sed -i 's/^#Server/Server/' 'mirrorlist'
-awk '/^## '"$COUNTRY"'$/{f=1; next}f==0{next}/^$/{exit}{print substr($0, 1);}' 'mirrorlist' > "$TMPFILE"
-rankmirrors -n 10 "$TMPFILE" > /etc/pacman.d/mirrorlist
-pacman -Syy
+sed -i "s/#IgnorePkg    = IgnorePkg    = discover plasma-welcome" "/etc/pacman.conf" #useless on arch kde
 
 #Install KDE Plasma Desktop
 pacman -Sy --noconfirm plasma-meta plasma-wayland-session
 systemctl enable sddm.service
-
-
