ef0a3de4db0deaf90834ce40d2078b4052f3febf
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-07-04 23:23:09 +0530

    Add waterfox Package & Typo Fix

--- kde-arch/install-kde.sh
+++ kde-arch/install-kde.sh
@@ -27,22 +27,23 @@ prompt "Standard Username [asuna]: "
 read USERNAME
 USERNAME=${USERNAME:-asuna}
 
+prompt "User Password [yuuki]: "
+read -s USER_PASSWORD
+USER_PASSWORD=${USER_PASSWORD:-yuuki}
+
 prompt "User as Root $USERNAME [y/N]: "
 read USER_AS_ROOT
 [[ "$USER_AS_ROOT" != "y" ]] && USER_AS_ROOT=NO
 [[ "$USER_AS_ROOT" = "y" ]] && USER_AS_ROOT=Yes
-
-prompt "User Password [yuuki]: "
-read -s USER_PASSWORD
-USER_PASSWORD=${USER_PASSWORD:-yuuki}
 
 # Configuration
 echo ""
 echo ""
 printf "%-16s\t%-16s\n" "CONFIGURATION" "VALUE"
 printf "%-16s\t%-16s\n" "Username:" "$USERNAME"
-printf "%-16s\t%-16s\n" "User as Root:" "$USER_AS_ROOT"
 printf "%-16s\t%-16s\n" "User Password:" "`echo \"$USER_PASSWORD\" | sed 's/./*/g'`"
+printf "%-16s\t%-16s\n" "User as Root:" "$USER_AS_ROOT"
+
 echo ""
 prompt "Proceed? [y/N]: "
 read PROCEED
@@ -56,7 +57,7 @@ groupadd sudo
 
 # Setup user
 useradd -m "$USERNAME"
-"echo -e \"$USER_PASSWORD\n$USER_PASSWORD\" | passwd $USERNAME"
+echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USERNAME
 if [ "$USER_AS_ROOT" = "Yes" ];then
 usermod -aG sudo "$USERNAME" ;fi
 
@@ -65,11 +66,27 @@ echo "## Allow $USERNAME to execute any root command
 %$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # Pacman Configuration
-sed -i "s/#ParallelDownloads    = 5/ParallelDownloads    = 4/" "/etc/pacman.conf"
+sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 4/" "/etc/pacman.conf"
 sed -i "s/#Color/Color/" "/etc/pacman.conf"
-sed -i "s/#IgnorePkg    =/IgnorePkg    = discover plasma-welcome/" "/etc/pacman.conf" #useless on arch kde
+sed -i "s/#IgnorePkg   =/IgnorePkg   = discover plasma-welcome/" "/etc/pacman.conf" #useless on arch kde
 
 #Install KDE Plasma Desktop
-pacman -Sy --noconfirm plasma-meta plasma-wayland-session dolphin kate konsole zsh ark
+pacman -Sy --noconfirm plasma-meta plasma-wayland-session konsole
 systemctl enable sddm.service
 
+#Install Additional Utils
+pacman -Sy --noconfirm dolphin kate zsh ark
+
+# Install Waterfox-G-Kpe
+echo "
+## Prebuilt Waterfox Repo
+[home_hawkeye116477_waterfox_Arch]
+Server = https://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/Arch/$arch" >> /etc/pacman.conf
+#Install Key
+key=$(curl -fsSL https://download.opensuse.org/repositories/home:hawkeye116477:waterfox/Arch/$(uname -m)/home_hawkeye116477_waterfox_Arch.key)
+fingerprint=$(gpg --quiet --with-colons --import-options show-only --import --fingerprint <<< "${key}" | awk -F: '$1 == "fpr" { print $10 }')
+
+pacman-key --init
+pacman-key --add - <<< "${key}"
+pacman-key --lsign-key "${fingerprint}"
+pacman -Sy home_hawkeye116477_waterfox_Arch/waterfox-g-kpe
