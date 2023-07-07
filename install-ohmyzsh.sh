8acbfb647a2acd0665f5a3cf0970a4269668160e
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-07-07 14:39:58 +0530

    fix file locations

--- install-ohmyzsh.sh
+++ install-ohmyzsh.sh
@@ -2,16 +2,19 @@
 
 # Get Username
 USERNAME="$USER"
+echo "$USERNAME"
 
-set -ex
+set -x
+
 # Install ohmyzsh for user
-./ohmyzsh.sh
+echo "Installing for $USERNAME"
+sh ohmyzsh.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-cp -fv .zshrc ~/
-cp -fv .p10k.zsh
+cp -fv ~/git/config/.zshrc  ~/
+cp -fv ~/git/config/.p10k.zsh ~/
 
 # Install ohmyzsh for root
 sudo bash <<EOF
