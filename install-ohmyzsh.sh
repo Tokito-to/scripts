c1ad216fc7b58d935d189eb6c2116f3858769d8f
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-07-08 13:38:15 +0530

    fix typos

--- install-ohmyzsh.sh
+++ install-ohmyzsh.sh
@@ -11,7 +11,7 @@ sh ~/git/config/ohmyzsh.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 cp -fv ~/git/config/.zshrc  ~/
 cp -fv ~/git/config/.p10k.zsh ~/
 USER
