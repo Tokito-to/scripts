cddefe0a18899c955e381c7d3adc246a0e77e2a3
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-07-08 11:49:06 +0530

    fix not installing for user

--- install-ohmyzsh.sh
+++ install-ohmyzsh.sh
@@ -8,7 +8,7 @@ set -x
 
 # Install ohmyzsh for user
 echo "Installing for $USERNAME"
-sh ohmyzsh.sh
+sh ~/git/config/ohmyzsh.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
