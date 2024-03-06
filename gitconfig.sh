17a037609d6e98d98c726572450d0cbb27178798
Yukimitsu-Tokito <93600306+Yukimitsu-Tokito@users.noreply.github.com>
2024-03-06 19:16:11 +0530

    gitconfig: Update script
    made more verbose with checks

--- gitconfig.sh
+++ gitconfig.sh
@@ -2,14 +2,33 @@
 
 set -e
 
-#Get Username
-echo "Github username:"
+# Check package
+if [[ -z $(command -v jq) ]]; then
+  echo "jq Not Installed."
+  exit 1
+fi
+
+# Get Username
+echo -n "Github username: "
 read -r USERNAME
 
-#Get Github ID
+# Get Github ID
+echo "Fetching Id from Github..."
 GET_ID() { curl -s https://api.github.com/users/"$USERNAME" | jq '.id'; }
-ID=$(GET_ID) 
+ID=$(GET_ID)
 
-#Set Git Global Config
-git config --global user.email "$ID+$USERNAME@users.noreply.github.com"
-git config --global user.name "$USERNAME"
+# Check Username
+if [ "$ID" == "null" ]; then
+  echo "Invalid Username!"
+  exit 1
+fi
+
+# Set as Git Global Configs
+echo "Global user.email: $ID+$USERNAME@users.noreply.github.com"
+#git config --global user.email "$ID+$USERNAME@users.noreply.github.com"
+
+echo "Global user.name: $USERNAME"
+#git config --global user.name "$USERNAME"
+
+echo "nano as Default Editor for Git"
+#git config --global core.editor nano
