2c35196cdea0e23065d0b364d126bf5e4e322af3
Tokito_to <RustedSilver11@proton.me>
2025-09-10 19:20:54 +0530

    script: virtm gaurd echo commands
    - check if file exists before writing to it

--- archlinux/virtm.sh
+++ archlinux/virtm.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/bash
-
-set -e
+#!/usr/bin/bash -e
 
 # Run as Root
 if [[  $EUID -ne 0 ]];then
@@ -8,24 +6,27 @@ if [[  $EUID -ne 0 ]];then
     exit 1
 fi
 
-pacman -S virt-manager dnsmasq qemu-base qemu-desktop
+pacman -S --needed --noconfirm virt-manager dnsmasq qemu-base qemu-desktop
 
-# libvirtd config
+# libvirt
+sed -i "/uri_default/s/#//" /etc/libvirt/libvirt.conf
+
+# libvirtd
 sed -i "/unix_sock_group/s/#//" /etc/libvirt/libvirtd.conf
 sed -i "/unix_sock_ro_perms/s/#//" /etc/libvirt/libvirtd.conf
 
-# virtstoraged
-sed -i "/unix_sock_group/s/#//" /etc/libvirt/virtstoraged.conf
-sed -i "/unix_sock_ro_perms/s/#//" /etc/libvirt/virtstoraged.conf
+# network
+if [[ -f /etc/libvirt/network.conf ]]; then
+    echo -e "\nfirewall_backend = \"iptables\"" >> /etc/libvirt/network.conf
+fi
 
-# virtnetwork
-echo -e "\nfirewall_backend=iptables" /etc/libvirt/network.conf
-
+# Qemu
+if [[ -f /etc/libvirt/qemu.conf ]]; then
+    echo "###################################################################
 # Qemu user
-echo "###################################################################
-# Qemu user
-user = \"$(whoami)\"
+user = \"${USER}\"
 group = \"libvirt\"" >> /etc/libvirt/qemu.conf
+fi
 
 # enable ip forwarding for ufw firewall
 sed -i "/net\/ipv4\/ip_forward=1/s/#//" /etc/ufw/sysctl.conf
@@ -33,7 +34,7 @@ sysctl -p /etc/ufw/sysctl.conf
 ufw reload
 
 # add user to groups
-usermod -aG qemu,libvirt-qemu,libvirt,kvm "$(whoami)"
+usermod -aG qemu,libvirt-qemu,libvirt,kvm "${USER}"
 
 # Enable systemd services
 # qemu interface network nodedev storage
@@ -41,5 +42,6 @@ systemctl enable --now virtqemud.service virtlxcd.service \
   virtinterfaced.service virtnetworkd.service virtstoraged.service \
   virtnodedevd.service
 
-echo "Virt-Manager Insalled Reboot and launch virt-manager --connect qemu:///system"
+echo "Virt-Manager Insalled Reboot and launch virt-manager"
 echo "Run virsh net-start default to startup NAT"
+
